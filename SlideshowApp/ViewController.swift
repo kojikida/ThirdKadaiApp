c209065640d810811ad08aa9e937157d21ac3cc5
kojikida <koji.in.kiribati@gmail.com>
2020-03-16 14:53:26 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Koji Kida on 2020/03/16.
+//  Copyright © 2020 Koji Kida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
